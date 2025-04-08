f291ee9dac9b577db0e3852ab45e51789fb3de62
NK-SAT <nk.sat.27@gmail.com>
2025-04-08 14:50:52 +0530

    Mobile-commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <App />
   </React.StrictMode>
 );
